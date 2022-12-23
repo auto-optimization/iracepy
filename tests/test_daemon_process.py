f3ed0869fe4bcb124539c91366a12c3bb8a259cc
Deyao Chen <chendeyao000@gmail.com>
2022-12-23 14:40:51 +0800

    add test_daemon_process

---
+++ tests/test_daemon_process.py
@@ -0,0 +1,35 @@
+# This test is written because daemonic processes are not allowed to have children
+# and multiprocessing.Pool uses daemonic process.
+import numpy as np 
+from irace import irace
+import pandas as pd
+from multiprocessing import Process
+
+import json
+def target_runner(experiment, scenario):
+    Process(target=lambda x: x, args=(1,)).start()
+    return dict(cost=experiment['configuration']['one'])
+
+
+params = '''
+one "" r (0, 1)
+'''
+
+defaults = pd.DataFrame(data=dict(
+    one=[0.6]
+))
+
+scenario = dict(
+    instances = np.arange(10),
+    maxExperiments = 96,
+    debugLevel = 0,
+    parallel = 2,
+    logFile = ""
+)
+
+
+def test():
+    tuner = irace(scenario, params, target_runner)
+    tuner.set_initial(defaults)
+    best_conf = tuner.run()
+    print(best_conf)
